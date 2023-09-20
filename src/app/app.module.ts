f35ce9b92a12d9c6508bfd889abcf0b57fd78b03
Kevin Caron <kevin.caron.91@gmail.com>
2023-09-20 11:19:28 +0200

    questO2 binfing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BlockComponent } from '../app/block/block.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, BlockComponent, UserProfileComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [NgModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
